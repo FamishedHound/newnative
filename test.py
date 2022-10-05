c1f5f702b242b3c3e0fc5d9ab5dff037e778d135
unknown <l.pelcner@lancaster.ac.uk>
2022-10-05 20:31:03 +0200

    Add introduction

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ matplotlib.use('Agg')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 pd.set_option('display.max_colwidth', None)
 header = st.container()
+introduction = st.container()
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 
@@ -27,7 +28,7 @@ def create_tweets_string_for_sentiment(list_of_tweets):
         str += f"{idx + 1}. \"{tweet}\"\n"
     return str
 
-
+@st.cache
 def gpt_3_request(str):
     response = openai.Completion.create(
         model="text-davinci-002",
@@ -40,7 +41,7 @@ def gpt_3_request(str):
     )
     return response["choices"][0]["text"].replace(",", "\n")
 
-
+@st.cache
 def fetch_financial_data(currency, when):
     date_time = datetime.datetime.strptime(when, '%Y-%m-%d').date()
     start_date = date_time - datetime.timedelta(days=30)
@@ -72,8 +73,8 @@ def run_analysis(tweet, financial_data, effect_day, start_date, end_date):
         else:
             sentiment = "neutral"
 
-        st.markdown(f"## The sentiment is {sentiment} \n"
-                    f" ### If the sentiment is Positive we expect uplift if "
+        st.markdown(f"### The sentiment is {sentiment} \n"
+                    f" #### If the sentiment is Positive we expect uplift if "
                     f"negative pulldown and if neutral no effect or minimal effect")
         #
         # string_date = df[df['tweet'] == tweet].date.iloc[0].split(" ")[0].split("-")
@@ -94,7 +95,21 @@ def run_analysis(tweet, financial_data, effect_day, start_date, end_date):
         st.markdown(f" ## {ci.summary('report')}")
         st.pyplot(ci.plot())
         sleep(10)
+with introduction:
+    st.title("# How your twitter idol is affecting crypto/currencies")
 
+    st.markdown("## Goal of the app is to help people that influencing "
+                "currency or crypto prices via Twitter. Event driven day trading is a popular"
+                "method for day traders. Articles,events as well as twitter post can be translated"
+                "into accurate signals. By using GPT-3 we are able to determine the sentiment of the"
+                "tweets which can tell us if positive tweets regarding certain currency are influencing the "
+                "price. In order to know if the event had effect we need time series to predict what would"
+                "have happened if such event had no place. That is why I trained Bayesian Structural Time Series"
+                "model that can deliver. With Prediction (Shown as --- line) and what happened in reality (shown as y)"
+                "We can simulate statistical test or A/B test and know the magnitude of the effect."
+                ""
+                "Please feel free to play around with different tweets"
+                "")
 tweet = st.sidebar.text_input("Please provide content of the tweet", "One word: Doge")
 date = st.sidebar.text_input("When provide a date when the tweet happened (e.g. 2022-01-02)", "2020-12-20")
 currency = st.sidebar.text_input("What currency you want to compare to USD (e.g. DOGE, PLN, BTC)", "DOGE")
