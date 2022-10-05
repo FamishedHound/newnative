989b3a9c01b3fe5d0a4a8adf3915749c24d8c385
unknown <l.pelcner@lancaster.ac.uk>
2022-10-05 20:38:53 +0200

    Add some minor text improvements

--- test.py
+++ test.py
@@ -98,18 +98,19 @@ def run_analysis(tweet, financial_data, effect_day, start_date, end_date):
 with introduction:
     st.title("How your twitter idol is affecting crypto/currencies")
 
-    st.markdown("Goal of the app is to help people that influencing "
+    st.markdown("Goal of the app is to help people that are interested in targeting people that are influencing "
                 "currency or crypto prices via Twitter. Event driven day trading is a popular"
-                "method for day traders. Articles,events as well as twitter post can be translated"
+                "method for day traders. Articles,events as well as twitter posts can be translated"
                 "into accurate signals. By using GPT-3 we are able to determine the sentiment of the"
                 "tweets which can tell us if positive tweets regarding certain currency are influencing the "
                 "price. In order to know if the event had effect we need time series to predict what would"
                 "have happened if such event had no place. That is why I trained Bayesian Structural Time Series"
-                "model that can deliver. With Prediction (Shown as --- line) and what happened in reality (shown as y)"
-                "We can simulate statistical test or A/B test and know the magnitude of the effect."
+                "model that can deliver just that. With Prediction (Shown as --- line) and what happened in reality (shown as y)"
+                "We can simulate statistical test or A/B test and know the magnitude of the effect as well as probability/uncertainty "
+                "of the effect."
                 ""
                 "Please feel free to play around with different tweets"
-                "")
+                "Note all currencies are compared again US dollar so the metric used is your_currency/USD")
 tweet = st.sidebar.text_input("Please provide content of the tweet", "One word: Doge")
 date = st.sidebar.text_input("When provide a date when the tweet happened (e.g. 2022-01-02)", "2020-12-20")
 currency = st.sidebar.text_input("What currency you want to compare to USD (e.g. DOGE, PLN, BTC)", "DOGE")
