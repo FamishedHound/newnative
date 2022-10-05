c2162747d43ad0245f8b04d05d825f166bf72d1d
unknown <l.pelcner@lancaster.ac.uk>
2022-10-05 20:11:07 +0200

    Adding secret

--- test.py
+++ test.py
@@ -98,7 +98,7 @@ def run_analysis(tweet, financial_data, effect_day, start_date, end_date):
 tweet = st.sidebar.text_input("Please provide content of the tweet", "One word: Doge")
 date = st.sidebar.text_input("When provide a date when the tweet happened (e.g. 2022-01-02)", "2020-12-20")
 currency = st.sidebar.text_input("What currency you want to compare to USD (e.g. DOGE, PLN, BTC)", "DOGE")
-openai.api_key = api_key
+
 state = st.sidebar.button("Let's analyze this tweet !")
 
 if state:
